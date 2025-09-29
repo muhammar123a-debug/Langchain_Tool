5ac2b04006a7551318ad592505c6e8b32cbf2783
muhammar123a-debug <muhammar123a@gmail.com>
2025-09-29 20:19:28 +0500

    add practice code

--- Basic.py
+++ Basic.py
@@ -1,13 +1,20 @@
 from langchain.tools import tool
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain_google_genai import ChatGoogleGenerativeAI
+import os
+from dotenv import load_dotenv
 
-@tool
-def claculator(expression: str) -> str:
-    """Solve a basic math expression and return result"""
-    try:
-        result = eval(expression)
-        return str(result)
-    except Exception as e:
-        return f"Error: {e}"
+# @tool
+# def claculator(expression: str) -> str:
+#     """Solve a basic math expression and return result"""
+#     try:
+#         result = eval(expression)
+#         return str(result)
+#     except Exception as e:
+#         return f"Error: {e}"
 
-print(claculator("2+5*3"))
-print(claculator("100/4"))
+# print(claculator("2+5*3"))
+# print(claculator("100/4"))
+
+# basic practice
